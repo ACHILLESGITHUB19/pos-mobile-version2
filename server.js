b7d372b9b064458bcfd2a1459107c6e7003a1fde
Archilles Jaspher Silawan Baltazar <155724017+ACHILLESGITHUB19@users.noreply.github.com>
2026-01-04 08:16:10 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 /* ================= IMPORTS ================= */
 import express from "express";
-import bcrypt from "bcryptjs";
+import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
